075fc5ba8cb0c0fffa394d44bd676b184a5894e7
aakash <aakashbhatia030@gmail.com>
2022-01-30 20:09:46 +0530

    Update smtp.php

--- smtp.php
+++ smtp.php
@@ -34,13 +34,14 @@ $retval = mail($to,$subject,$message,$headers);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
   curl_setopt($ch, CURLOPT_POST, 1);
   curl_setopt($ch, CURLOPT_POSTFIELDS, "{\n\t\t\"Messages\":[\n\t\t\t\t{\n\t\t\t\t\t\t\"From\": {\n\t\t\t\t\t\t\t\t\"Email\": \"dumyui81@gmail.com\",\n\t\t\t\t\t\t\t\t\"Name\": \"Mailjet Pilot\"\n\t\t\t\t\t\t},\n\t\t\t\t\t\t\"To\": [\n\t\t\t\t\t\t\t\t{\n\t\t\t\t\t\t\t\t\t\t\"Email\": \"aakashbhatia030@gmail.com\",\n\t\t\t\t\t\t\t\t\t\t\"Name\": \"passenger 1\"\n\t\t\t\t\t\t\t\t}\n\t\t\t\t\t\t],\n\t\t\t\t\t\t\"Subject\": \"Your email flight plan!\",\n\t\t\t\t\t\t\"TextPart\": \"Dear passenger 1, welcome to Mailjet! May the delivery force be with you!\",\n\t\t\t\t\t\t\"HTMLPart\": \"<h3>Dear passenger 1, welcome to <a href=\"https://www.mailjet.com/\">Mailjet</a>!</h3><br />May the delivery force be with you!\"\n\t\t\t\t}\n\t\t]\n\t}");
-  curl_setopt($ch, CURLOPT_USERPWD, $_ENV["640cc816ae9eb44cdb24fba03f76fae5"] . ':' . $_ENV["22e821b5edd3a898deb4ae9eed50b574"]);
+  curl_setopt($ch, CURLOPT_USERPWD, "640cc816ae9eb44cdb24fba03f76fae5" . ':' . "22e821b5edd3a898deb4ae9eed50b574");
   
   $headers = array();
   $headers[] = 'Content-Type: application/json';
   curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
   
   $result = curl_exec($ch);
+  echo $result;
   if (curl_errno($ch)) {
       echo 'Error:' . curl_error($ch);
   }
